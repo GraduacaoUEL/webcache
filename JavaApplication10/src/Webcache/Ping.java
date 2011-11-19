34a6109164697e36bba309350fed442f7efff5d4
tadeusrox@gmail.com <tadeusrox@gmail.com@c2f64d62-c27b-3225-2371-61bbf3db70b4>
2011-11-19 01:57:51 +0000

    Fiz o ping de um modo menos gambiarranto galere... usei o inet ... isreacheable e um cronometrozinho vagabundo não é 100% maaas já é um começo/ um "tá bom assim"

--- JavaApplication10/src/Webcache/Ping.java
+++ JavaApplication10/src/Webcache/Ping.java
@@ -9,71 +9,34 @@ package webcache;
  * @author Vinicius
  */
 import java.io.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * A Java ping class.
  * Created by Alvin Alexander, devdaily.com.
  */
 public class Ping {
-
-    public void start()
-            throws IOException {
-        // create the ping command as a list of strings
-        Ping ping = new Ping();
-        List<String> commands = new ArrayList<String>();
-        commands.add("ping ");
-        commands.add("www.google.com");
-        ping.doCommand(commands);
-    }
-
-    /**
-     * Provide the command you want to run as a List of Strings. Here's an example:
-     * 
-     * List<String> commands = new ArrayList<String>();
-     * commands.add("/sbin/ping");
-     * commands.add("-c");
-     * commands.add("5");
-     * commands.add("www.google.com");
-     * exec.doCommand(commands);
-     * 
-     * @param command The command you want to execute, provided as List<String>.
-     * @throws IOException This exception is thrown so you will know about it, and can deal with it.
-     */
-    public void doCommand(List<String> command)
-            throws IOException {
-        String s = null;
-
-        ProcessBuilder pb = new ProcessBuilder(command);
-        Process process = pb.start();
-
-        BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
-        // read the output from the command
-        System.out.println("Here is the standard output of the command:\n");
-        while ((s = stdInput.readLine()) != null) {
-            System.out.println(parse(s));
-        }
-
-        // read any errors from the attempted command
-        System.out.println("Here is the standard error of the command (if any):\n");
-        while ((s = stdError.readLine()) != null) {
-            System.out.println(s);
-        }
-    }
-
-    public String parse(String str) {
-        String dados[] = new String[1000];
-        dados = str.split(" ");
-        String strReturn = new String();
-        for (int i = 0; i < dados.length; i++) {
-            if(dados[i].endsWith("ms"))
-            {
-                strReturn = dados[i].replace("ms", "");
-                strReturn = strReturn.replace("tempo=","");
+    
+    public long pingEmMS(String ip) 
+    {
+        long data = System.currentTimeMillis();
+            try {
+                InetAddress.getByName(ip).isReachable(0);
+            } catch (IOException ex) {
+                Logger.getLogger(Ping.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }
-        return strReturn;
+         long data2 = System.currentTimeMillis();
+            return data2 - data ;
+    }
+    
+        public static void main(String[] args) {
+        Ping p = new Ping();
+        System.out.println(p.pingEmMS("127.0.0.1"));
+        System.out.println(p.pingEmMS("www.google.com.br"));
+
     }
 }
